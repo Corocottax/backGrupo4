0ff461911f2df350a1c51c9db7c46e1a0bdf0fa9
Corocottax <santicorocottax@gmail.com>
2022-07-22 20:28:59 +0200

    first commit

---
+++ src/api/users/users.model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+    userName: { type: String, trim: true, required: true, unique: true },
+    age: { type: Number, trim: true, required: true},
+    house: { type: String, trim: true, required: true},
+    password: { type: String, trim: true, required: true },
+    phone: { type: String, trim: true, required: true }
+}, { timestamps: true, collection: 'users'})
+
+
+userSchema.pre("save", function (next) {
+    this.password = bcrypt.hashSync(this.password, 10);
+    next();
+});
+const User = mongoose.model('users', userSchema)
+module.exports = User
