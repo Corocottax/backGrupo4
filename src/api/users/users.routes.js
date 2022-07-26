ddf2aa9d30dcb04354e9d53128b955e7cd0426fb
Corocottax <santicorocottax@gmail.com>
2022-07-26 21:06:05 +0200

    type string age

--- src/api/users/users.routes.js
+++ src/api/users/users.routes.js
@@ -5,7 +5,7 @@ const { isAuth } = require("../../middlewares/auth");
 UserRoutes.post('/', postNewUser);
 UserRoutes.post('/login', loginUser);
 UserRoutes.post('/logout', logoutUser);
-UserRoutes.get('/', [isAuth], getUsers);
+UserRoutes.get('/', getUsers);
 UserRoutes.get('/:id', [isAuth], getUser);
 UserRoutes.patch('/:id', [isAuth], patchUser);
 UserRoutes.delete('/:id', [isAuth], deleteUser);
