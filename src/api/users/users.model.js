0f5686e4c2c5f7e9e5e7d8f997e2c5c262a9667b
Corocottax <santicorocottax@gmail.com>
2022-07-26 20:46:40 +0200

    type string age

--- src/api/users/users.model.js
+++ src/api/users/users.model.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     userName: { type: String, trim: true, required: true, unique: true },
     email: { type: String, trim: true, required: true, unique: true },
-    age: { type: Number, trim: true, required: true},
+    age: { type: String, trim: true, required: true},
     house: { type: String, trim: true, required: true},
     password: { type: String, trim: true, required: true },
     phone: { type: String, trim: true, required: false }
